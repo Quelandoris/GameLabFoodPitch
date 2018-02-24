2bb8663143659aa3efb767e2ff285a5a010db058
Quelandoris <aws140230@utdallas.edu>
2018-02-24 14:55:25 -0600

    Buying tiles mostly working

---
+++ Culinary Domination/Assets/Event Manager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour {
+
+
+	//Events
+	public delegate void TurnEvent();
+	public static event TurnEvent TurnChange; //Call whenever a turn ends
+	// Use this for initialization
+	void OnEnable() {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
